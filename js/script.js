5e96625e4f30a9743767ba01dbcd0ca9cd1e21af
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-03-09 18:19:27 +0000

    basicmath terminal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+  function doMathClicked() {
+    document .getElementById('perimeter').innerHTML = "<p>Perimeter = " + (10+5+10+5) + " cm</p>"
+    document .getElementById('area').innerHTML = "<p>Area = " + (10*5) + " ㎠</p>"
+  }
