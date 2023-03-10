047734b7751eb48e5ceefbb1f46779d6b3c30881
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-03-10 18:04:47 +0000

    added extra

---
+++ go_app/main.go
@@ -0,0 +1,19 @@
+//Created By: Lamees Hemdan
+//Created on: March 2023
+//
+//This program calcualtes area and perimeter of a rectangle
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//This calculates Area and Perimeter, dimensions of 5cm and 3cm
+	fmt.Println("The dimensions are: 5 cm and 3 cm")
+	fmt.Println("Area is", 5*3, "cm²")
+	fmt.Println("Perimeter is", 2*(5+3), "cm")
+
+	fmt.Println("\nDone.") 
+}
